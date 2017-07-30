210139457a05a47d977f94b9d7b1aa9c4838f2e0
chuniee0126 <chunie0126@gmail.com>
2017-07-30 17:01:02 +0900

    modified : 주석 추가

--- Single_Neuron/Single_Neuron.hpp
+++ Single_Neuron/Single_Neuron.hpp
@@ -23,22 +23,22 @@
 
 namespace dlop {
 /*
- * @ Definition   : Neuron 구성 요소 정의 (without ActivationFunction)
+ * @Definition   : Neuron 구성 요소 정의 (without ActivationFunction)
  *
- * @ Structure    : Single_Neuron,
- *                  Propagation,
+ * @Structure    : Single_Neuron,
+ *                 Propagation,
  *
- * @ Member       : Weight,
- *                  Bias,
- *                  input   // for ForwardPropagation
- *                  output  // for BackPropagation
+ * @Member       : Weight,
+ *                 Bias,
+ *                 input   // for ForwardPropagation
+ *                 output  // for BackPropagation
  *
  */
 
 class SingleNeuron {
 private:
 
-    // @ Member
+    // @Member
     double *m_Weight;
     double *m_Bias;
     double *m_input;
@@ -68,13 +68,31 @@ public:
         delete m_output;
     }
 
+    /*
+     * @brief Initialize Weight and Bias
+     */
     void   InitializeWeightandBias();
-    double MakeSigma(const double& p_input);            // Sigma = Weight *
-                                                        // input + bias
-    void   UpdateWeightandBias(const double& LearningRate,
-                               const double& Gradient); // Learning Graph
 
-    // Getter
+    /*
+     * @brief Calculate Sigma = Weight * Input + bias
+     *
+     * @param p_input
+     *        외부로부터 들어온 input 값을 member에 저장하고 이를 사용한다.
+     */
+    double MakeSigma(const double& p_input);
+
+    /*
+     * @brief As Result of BackPropagation, update Weight and Bias
+     *
+     * @param LearningRate
+     *        Weight과 Bias 변화에 영향을 미치는 정도
+     * @param Gradient
+     *        계산 된 Gradient 값
+     */
+    void   UpdateWeightandBias(const double& LearningRate,
+                               const double& Gradient);
+
+    // output member 주소 반환
     double* GetOutput() {
         return m_output;
     }
@@ -90,7 +108,7 @@ public:
 
 
 /*
- * @ Definition  : ActivationFunction class 정의
+ * @Definition : ActivationFunction class 정의
  *
  */
 class ActivationFunction {
@@ -100,6 +118,14 @@ public:
 
     ~ActivationFunction() {}
 
+    /*
+     * @brief With Sigma, Calculate output on RelU ActivationFunction
+     *
+     * @param p_Neuron
+     *        Neuron에서 계산되는 Sigma 값 및 output 주소 참조
+     * @param p_input
+     *        외부에서 주어지는 input 값(주로 Sigma를 계산하는 데에 쓰인다)
+     */
     void ReLU(SingleNeuron& p_Neuron,
               const double& p_input);
 
@@ -108,7 +134,7 @@ public:
 
 
 /*
- * @ Definition   : BackPropagation class 정의
+ * @Definition : BackPropagation class 정의
  *
  */
 class BackPropagation {
@@ -118,9 +144,26 @@ public:
 
     ~BackPropagation() {}
 
-    void                GradientDescent(SingleNeuron& p_Neuron,
-                                        const double& desired_output,
-                                        const double& LearningRate);
+    /*
+     * @brief Optimizer 방식으로, GradientDescent 방식 차용
+     *
+     * @param p_Neuron
+     *        Neuron에서 output 주소와 UpdateWeightandBias함수 참조
+     * @param desired_output
+     *        외부에서 예상하는 desired output으로 Loss를 계산하기 위해 사용
+     * @param LearningRate
+     *        UpdateWeightandBias함수에서 사용하기 위한 용도
+     */
+    void GradientDescent(SingleNeuron& p_Neuron,
+                         const double& desired_output,
+                         const double& LearningRate);
+
+    /*
+     * @brief ActivationFunction 중 ReLU함수의 Gradient 계산에 사용
+     *
+     * @param output
+     *        ForwardPropagation에서 만들어진 output을 계산에 참조
+     */
     inline const double getReLUGradient(const double& output);
 };
 } // namespace dlop
