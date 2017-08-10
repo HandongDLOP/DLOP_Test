3e27601b3ada0810d8b75b911e9676d163f53ba2
chuniee0126 <chunie0126@gmail.com>
2017-08-10 20:33:54 +0900

    modified : main cpp file

--- Multilayer_Perceptron/HGUNeuralNetwork_main.cpp
+++ Multilayer_Perceptron/HGUNeuralNetwork_main.cpp
@@ -15,41 +15,26 @@ int main(int argc, char const *argv[]) {
 
     dlop::HGUNeuralNetwork my_Neuralnet(2);
 
-    my_Neuralnet.AllocEachLayer(0, 2, 3);
+    my_Neuralnet.AllocEachLayer(0, 2, 4);
 
-    my_Neuralnet.AllocEachLayer(1, 3, 1);
+    my_Neuralnet.AllocEachLayer(1, 4, 1);
 
     for (int i = 0; i < 1000000; i++) {
         std::cout << "count: " << i << '\n';
 
-        FILE *pFile = fopen("Weight.csv", "at");
-
-        fprintf(pFile, "\n");
-
-        // std::cout << "input " << input[i % 4][0] << "," << input[i % 4][1] <<
-        // " : ";
-
         my_Neuralnet.ComputeGradient(input[i % 4], DesiredOutput[i % 4]);
+
         my_Neuralnet.UpdateWeight(0.3);
-
-        if (i % 10 == 9) {
-            my_Neuralnet.Propagate(input[0]);
-            my_Neuralnet.Propagate(input[1]);
-            my_Neuralnet.Propagate(input[2]);
-            my_Neuralnet.Propagate(input[3]);
-        }
-
-        fprintf(pFile, "\n");
-
-        fclose(pFile);
     }
 
     std::cout << "Result: " << '\n';
 
-    my_Neuralnet.Propagate(input[0]);
-    my_Neuralnet.Propagate(input[1]);
-    my_Neuralnet.Propagate(input[2]);
-    my_Neuralnet.Propagate(input[3]);
+    my_Neuralnet.GetResult(input[0]);
+    my_Neuralnet.GetResult(input[1]);
+    my_Neuralnet.GetResult(input[2]);
+    my_Neuralnet.GetResult(input[3]);
+
+    std::cout << "-----------FINISH-----------" << '\n';
 
     return 0;
 }
